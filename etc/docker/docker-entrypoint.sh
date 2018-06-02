8cd40e47ce8a5fb911ac48bb6e094c37950b5f56
Michail Brynard <mbrynard@gmail.com>
2018-06-02 15:18:37 +0200

    Update deployment configs

--- etc/docker/docker-entrypoint.sh
+++ etc/docker/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-perl -pi -e 's/\{\{REHIVE_API_URL\}\}/$ENV{'REHIVE_API_URL'}/g' /usr/share/nginx/html/static/js/*
+#perl -pi -e 's/\{\{REHIVE_API_URL\}\}/$ENV{'REHIVE_API_URL'}/g' /usr/share/nginx/html/static/js/*
 
 exec "$@"
